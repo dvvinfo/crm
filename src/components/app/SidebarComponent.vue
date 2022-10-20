23b0f31f6576fd28c2845af0efd64c3c68c97707
dvvinfo <dvvinfo@mail.ru>
2022-10-20 15:20:52 +0300

    added Navbar and Sidebar components

---
+++ src/components/app/SidebarComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="sidenav app-sidenav " :class="{open: modelValue}">
+    <li v-for="link in links" :key="link.url">
+      <router-link
+        href="#"
+        active-class="active"
+        :to="link.url"
+        :exact="link.exact"
+        class="waves-effect waves-orange pointer"
+      >
+        {{ link.title }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  data: () => ({
+    links: [
+      { title: "Счет", url: "/", exact: true },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planning" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категории", url: "/categories" },
+    ],
+  }),
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    meta: { layout: "main" },
+    component: () => import("../views/HomeView.vue"),
   },
   {
     path: "/login",
