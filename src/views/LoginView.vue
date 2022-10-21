64151fc475944630bed72248e8ee687ad82dfb85
dvvinfo <dvvinfo@mail.ru>
2022-10-21 13:21:26 +0300

    added validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,39 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate" />
+        <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{ invalid: v$.email.$error }"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          class="helper-text invalid"
+          v-for="error of v$.email.$errors"
+          :key="error.$uid"
+        >
+          {{ printErrorEmail(error.$validator, error.$params) }}
+        </small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input
+          id="password"
+          type="password"
+          class="validate"
+          v-model.trim="password"
+          :class="{ invalid: v$.password.$error }"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          class="helper-text invalid"
+          v-for="error of v$.password.$errors"
+          :key="error.$uid"
+        >
+          {{ printErrorPassword(error.$validator, error.$params) }}
+        </small>
       </div>
     </div>
     <div class="card-action">
@@ -23,14 +46,64 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
 
 <script>
-export default {};
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, minLength } from "@vuelidate/validators";
+export default {
+  name: "loginView",
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  data: () => ({
+    email: "",
+    password: "",
+  }),
+  validations: () => ({
+    email: { email, required, minLength: minLength(3) },
+    password: { required, minLength: minLength(6) },
+  }),
+  methods: {
+    submitHandler() {
+      console.log(this.v$.password);
+      this.v$.$touch();
+      if (this.v$.$error) return;
+
+      const formData = {
+        email: this.email,
+        password: this.password
+      }
+      console.log(formData)
+
+      this.$router.push("/");
+    },
+
+    printErrorEmail($name) {
+      if ($name === "required") {
+        return "Поле не должно быть пустым";
+      } else if ($name === "minLength") {
+        return "Введите коректный Email";
+      }
+    },
+    printErrorPassword($name, $params) {
+      if ($name === "required") {
+        return "Введите пароль";
+      } else if ($name === "minLength") {
+        return (
+          "Минимальная длина пароля должна быть" +
+          " " +
+          $params.min +
+          " символов "
+        );
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
